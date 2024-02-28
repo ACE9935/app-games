e00d9acd2f7f8691a8863c31ca6fe009ec187b8e
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-02-29 00:02:19 +0100

    update

--- src/gamesArray.js
+++ src/gamesArray.js
@@ -1,22 +1,22 @@
 export const gamesArray = [
     {
       name: "Hangman",
-      image: "/game1.png", // Replace with actual image URL
+      image: "./game1.png", // Replace with actual image URL
       description: "A classic word-guessing game where players try to guess a hidden word by suggesting letters."
     },
     {
       name: "Typeracer",
-      image: "/game3.png", // Replace with actual image URL
+      image: "./game3.png", // Replace with actual image URL
       description: "A typing competition game where players race against each other to type a given text accurately and quickly."
     },
     {
       name: "Match Game",
-      image: "/game2.png", // Replace with actual image URL
+      image: "./game2.png", // Replace with actual image URL
       description: "A memory game where players flip cards to find matching pairs within a set of facedown cards."
     },
     {
       name: "Rock-Paper-Scissors",
-      image: "/game4.png", // Replace with actual image URL
+      image: "./game4.png", // Replace with actual image URL
       description: "A hand game usually played between two people, where each player simultaneously forms one of three shapes with their hand."
     }
   ];
